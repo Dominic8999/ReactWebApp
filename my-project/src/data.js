9e5e1d7aa26bfb629b1c4449a5439bf3a3ce38e8
Dominic8999 <dominic3331@fedora.email>
2024-09-03 23:03:36 +0200

    Add catto.svg image

--- my-project/src/data.js
+++ my-project/src/data.js
@@ -1,10 +1,18 @@
+import doggie from './assets/doggie.svg'
+
 let component_1, component_2;
 
 let components =[
     component_1 = {
-        title: "a doggie"
+        title: "a doggie",
+        image: doggie,
+        description: "A doggie likes to play with bones"
     },
     component_2 = {
-        title: "catto"
+        title: "catto",
+        image: "",
+        description: "A catto likes to play with fishes"
     }
 ]
+
+export default components
