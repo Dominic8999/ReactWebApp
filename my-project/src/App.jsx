8bc133c68383b3b638ed3fc7efc8e1bc082644dc
Dominic8999 <dominic3331@fedora.email>
2024-09-03 16:45:38 +0200

    New Card looks

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,9 +3,6 @@ import Card from './Components/Card'
 function App() {
   return (
     <>
-      <p className='text-3xl font-bold underline'>
-        Hello World!
-      </p>
       <Card>
         <p>Hello World</p>
       </Card>
--- my-project/src/Components/Card.jsx
+++ my-project/src/Components/Card.jsx
@@ -1,14 +1,14 @@
 
 function Card({children}){
     return(
-        <>
+        <div className="container mx-auto box-border h-32 w-32 px-4 m-56 border-4">
             <div className="border-y-sky-100 text-green-500 underline">
                 <p>Hello World</p>
             </div>
             <div className="underline text-blue-900">
                 {children}
             </div>
-        </>
+        </div>
     )
 }
 
