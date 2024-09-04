cc2878292e52a88e91c2c501ae5ae3d1bda0d9b2
Dominic8999 <dominic3331@fedora.email>
2024-09-04 22:07:49 +0200

    added styling to list elements

--- my-project/src/Components/List.jsx
+++ my-project/src/Components/List.jsx
@@ -1,7 +1,7 @@
 export default function List(prop){
    
     return(
-        <li>
+        <li className="bg-blue-500 text-white text-center rounded-sm p-2 shadow-lg">
             {prop.text}
         </li>
     )
